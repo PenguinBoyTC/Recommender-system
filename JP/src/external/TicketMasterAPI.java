84c8138ed0d816ddf6acbfdae5cd86e3b440e698
Chong Tan <31427334+PenguinBoyTC@users.noreply.github.com>
2019-03-16 16:07:36 -0500

    Add getAddress, getItemList ...functions

--- JP/src/external/TicketMasterAPI.java
+++ JP/src/external/TicketMasterAPI.java
@@ -8,15 +8,25 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import entity.Item;
+import entity.Item.ItemBuilder;
 
 public class TicketMasterAPI {
 	private static final String URL = "https://app.ticketmaster.com/discovery/v2/events.json";
 	private static final String DEFAULT_KEYWORD = ""; // no restriction
 	private static final String API_KEY = "QNrVCUA1KcVuIj8Fa1An9op3R7TgA2zF";
-	public JSONArray search(double lat, double lon, String keyword) {
+	
+	//search data information by lat, lon, keyword
+	public List<Item> search(double lat, double lon, String keyword) {
 		if (keyword == null) {
 			keyword = DEFAULT_KEYWORD;
 		}
@@ -39,7 +49,7 @@ public class TicketMasterAPI {
 			System.out.println("Response code: " + responseCode);
 			
 			if (responseCode != 200) {
-				return new JSONArray();
+				return new ArrayList<>();
 			}
 			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			String line;
@@ -51,30 +61,136 @@ public class TicketMasterAPI {
 			JSONObject obj = new JSONObject(response.toString());
 			if (!obj.isNull("_embedded")) {
 				JSONObject embedded = obj.getJSONObject("_embedded");
-				return embedded.getJSONArray("events");
+				return getItemList(embedded.getJSONArray("events"));
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
-		return new JSONArray();
+		return new ArrayList<>();
 	}
-	
-	public static void main(String[] args) {
-		TicketMasterAPI tmApi = new TicketMasterAPI();
-		tmApi.queryAPI(29.682684, -95.295410);
+	//
+	private String getAddress(JSONObject event) throws JSONException {
+		if (!event.isNull("_embedded")) {
+			JSONObject embedded = event.getJSONObject("_embedded");
+			if (!embedded.isNull("venues")) {
+				JSONArray venues = embedded.getJSONArray("venues");
+				for (int i = 0; i < venues.length(); ++i) {
+					JSONObject venue = venues.getJSONObject(i);
+					StringBuilder addressBuilder = new StringBuilder();
+					if (!venue.isNull("address")) {
+						JSONObject address = venue.getJSONObject("address");
+						if (!address.isNull("line1")) {
+							addressBuilder.append(address.getString("line1"));
+						}
+						if (!address.isNull("line2")) {
+							addressBuilder.append(",");
+							addressBuilder.append(address.getString("line2"));
+						}
+						if (!address.isNull("line3")) {
+							addressBuilder.append(",");
+							addressBuilder.append(address.getString("line3"));
+						}
+					}
+					
+					if (!venue.isNull("city")) {
+						JSONObject city = venue.getJSONObject("city");
+						if (!city.isNull("name")) {
+							addressBuilder.append(",");
+							addressBuilder.append(city.getString("name"));
+						}
+					}
+					String addressStr = addressBuilder.toString();
+					if (!addressStr.equals("")) {
+						return addressStr;
+					}
+				}
+			}
+		}
+		return "";
 	}
+	private String getImageUrl(JSONObject event) throws JSONException {
+		if (!event.isNull("images")) {
+			JSONArray array = event.getJSONArray("images");
+			for (int i = 0; i < array.length(); i++) {
+				JSONObject image = array.getJSONObject(i);
+				if (!image.isNull("url")) {
+					return image.getString("url");
+				}
+			}
+		}
+		return "";
+	}
+	private Set<String> getCategories(JSONObject event) throws JSONException {
+		Set<String> categories = new HashSet<>();
+		if (!event.isNull("classifications")) {
+			JSONArray classifications = event.getJSONArray("classifications");
+			for (int i = 0; i < classifications.length(); ++i) {
+				JSONObject classification = classifications.getJSONObject(i);
+				if (!classification.isNull("segment")) {
+					JSONObject segment = classification.getJSONObject("segment");
+					if (!segment.isNull("name")) {
+						categories.add(segment.getString("name"));
+					}
+				}
+			}
+		}
+		return categories;
+	}
+	private List<Item> getItemList(JSONArray events) throws JSONException {
+		List<Item> itemList = new ArrayList<>();
+		for (int i = 0; i < events.length(); ++i) {
+			JSONObject event = events.getJSONObject(i);
+			ItemBuilder builder = new ItemBuilder();
+			if (!event.isNull("id")) {
+				builder.setItemId(event.getString("id"));
+			}
+			if (!event.isNull("name")) {
+				builder.setName(event.getString("name"));
+			}
+			if (!event.isNull("url")) {
+				builder.setUrl(event.getString("url"));
+			}
+			if (!event.isNull("distance")) {
+				builder.setDistance(event.getDouble("distance"));
+			}
+			if (!event.isNull("rating")) {
+				builder.setRating(event.getDouble("rating"));
+			}
+			builder.setAddress(getAddress(event))
+			.setCategories(getCategories(event))
+			.setImageUrl(getImageUrl(event));
+			itemList.add(builder.build());
+		}
+		return itemList;
+	}
+	// test data by lat and lon
 	private void queryAPI(double lat, double lon) {
-		JSONArray events = search(lat, lon, null);
+		List<Item> events = search(lat, lon, null);
 		
 		try {
-			for (int i = 0; i < events.length(); ++i) {
-				JSONObject event = events.getJSONObject(i);
-				System.out.println(event.toString(2));
+			for (int i = 0; i < events.size(); ++i) {
+				Item event = events.get(i);
+				System.out.println(event.toJSONObject());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	public static void main(String[] args) {
+		TicketMasterAPI tmApi = new TicketMasterAPI();
+		tmApi.queryAPI(29.682684, -95.295410);
+//		String s = "abcde";
+//		char[] c = new char[] {'a','a','c','d'};
+//		StringBuilder sb1 = new StringBuilder(s);
+//		StringBuilder sb2 = new StringBuilder(c.toString());
+//		System.out.println(sb1);
+//		System.out.println(sb2);
+//		System.out.println(new String(sb1));
+//		System.out.println(new String(sb2));
+//		System.out.println(sb1.toString());
+//		System.out.println(sb2.toString());
+	}
+	
 	
 }
